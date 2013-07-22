9e435ab0781323a0e4b1f28244d75e4816beb4af
Robert Millan <rmh@debian.org>
2013-07-22 19:55:32 +0000

    Make <sys/_types.h> and <machine/_types.h> usable on non-kFreeBSD.
    git-svn-id: svn://anonscm.debian.org/glibc-bsd/trunk/freebsd-glue@4789 ae44cbe4-c7d5-0310-ae45-95c72a56cd7d

---
+++ include/sys/_types.h
@@ -0,0 +1,15 @@
+#ifdef __FreeBSD_kernel__
+
+#include_next <sys/_types.h>
+
+#else
+
+#ifndef _SYS__TYPES_H_
+#define _SYS__TYPES_H_
+
+#include <sys/cdefs.h>
+#include <machine/_types.h>
+
+#endif /* !_SYS__TYPES_H_ */
+
+#endif
