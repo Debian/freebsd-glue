9cb5b03ff6f22d5844c8a00d4044d906c199ff31
Robert Millan <rmh@debian.org>
2013-08-04 21:49:45 +0000

    <sys/stat.h> implicitly includes <sys/time.h>.
    git-svn-id: svn://anonscm.debian.org/glibc-bsd/trunk/freebsd-glue@4868 ae44cbe4-c7d5-0310-ae45-95c72a56cd7d

--- include/sys/stat.h
+++ include/sys/stat.h
@@ -1,7 +1,12 @@
+#include_next <sys/stat.h>
+
 #ifndef _SYS_STAT_H_
 #define	_SYS_STAT_H_
-#include_next <sys/stat.h>
+
+#include <sys/time.h>
+
 #ifndef S_ISTXT
 #define S_ISTXT S_ISVTX
 #endif
-#endif
+
+#endif /* _SYS_STAT_H_ */
