3d239a8fd3ee3f234c5d312feb5667f4864ad7e7
Robert Millan <rmh@debian.org>
2013-08-06 15:23:38 +0000

    Add <sys/_stdint.h> to fix buildability of <libutil.h>.
    git-svn-id: svn://anonscm.debian.org/glibc-bsd/trunk/freebsd-glue@4875 ae44cbe4-c7d5-0310-ae45-95c72a56cd7d

---
+++ include/sys/_stdint.h
@@ -0,0 +1,14 @@
+#ifdef __FreeBSD_kernel__
+
+#include_next <sys/_stdint.h>
+
+#else
+
+#ifndef _SYS__STDINT_H_
+#define _SYS__STDINT_H_
+
+#include <stdint.h>
+
+#endif /* !_SYS__STDINT_H_ */
+
+#endif /* __FreeBSD_kernel__ */
