770af7dfa2d33908dfd812bced3a5c172c72d43b
Robert Millan <rmh@debian.org>
2013-08-06 16:14:28 +0000

    linkaddr.c is kFreeBSD-specific.
    git-svn-id: svn://anonscm.debian.org/glibc-bsd/trunk/freebsd-glue@4876 ae44cbe4-c7d5-0310-ae45-95c72a56cd7d

--- include/net/if_dl.h
+++ include/net/if_dl.h
@@ -1,6 +1,7 @@
 #include_next <net/if_dl.h>
 
-#ifdef __FreeBSD_kernel__
+#ifndef _FREEBSD_NET_IF_DL_H_
+#define _FREEBSD_NET_IF_DL_H_
 
 __BEGIN_DECLS
 void	link_addr(const char *, struct sockaddr_dl *);
