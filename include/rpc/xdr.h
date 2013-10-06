ab1a87f82f0cdd770b9d7bccb70db9ed9fdcded0
Robert Millan <rmh@debian.org>
2013-10-06 12:37:55 +0000

    Add <rpc/xdr.h>.
    git-svn-id: svn://anonscm.debian.org/glibc-bsd/trunk/freebsd-glue@4998 ae44cbe4-c7d5-0310-ae45-95c72a56cd7d

---
+++ include/rpc/xdr.h
@@ -0,0 +1,25 @@
+#include_next <rpc/xdr.h>
+
+#ifndef _RPC_XDR_H
+#define _RPC_XDR_H
+
+#include <sys/cdefs.h>
+
+/*
+ * These are XDR control operators
+ */
+
+#define        XDR_GET_BYTES_AVAIL     1
+#define        XDR_PEEK                2
+#define        XDR_SKIPBYTES           3
+
+struct xdr_bytesrec {
+	bool_t xc_is_last_record;
+	size_t xc_num_avail;
+};
+
+typedef struct xdr_bytesrec xdr_bytesrec;
+
+#define xdr_control(xdrs, req, op)	XDR_CONTROL(xdrs, req, op)
+
+#endif
