c5ba6a70856883f80fd195ecf0535793e3c9e85e
Robert Millan <rmh@debian.org>
2013-10-28 22:09:03 +0000

    Add <machine/elf.h> (with tricky build magic to generate ELF_ARCH).
    git-svn-id: svn://anonscm.debian.org/glibc-bsd/trunk/freebsd-glue@5106 ae44cbe4-c7d5-0310-ae45-95c72a56cd7d

---
+++ include/machine/elf.h
@@ -0,0 +1,33 @@
+#ifndef _MACHINE_ELF_H_
+#define _MACHINE_ELF_H_ 1
+
+#include <sys/elf32.h>
+#include <sys/elf64.h>
+
+#ifndef ELF_ARCH
+#include <machine/__get_elf_arch.h>	/* ELF_ARCH */
+#endif
+
+#ifndef ELF_TARG_DATA
+#include <endian.h>
+#if __BYTE_ORDER == __LITTLE_ENDIAN
+# define ELF_TARG_DATA	ELFDATA2LSB
+#elif __BYTE_ORDER == __BIG_ENDIAN
+# define ELF_TARG_DATA	ELFDATA2MSB
+#else
+# error wtf??
+#endif
+#endif
+
+#ifndef ELF_TARG_CLASS
+#include <link.h>
+#if __ELF_NATIVE_CLASS == 32
+# define ELF_TARG_CLASS	ELFCLASS32
+#elif __ELF_NATIVE_CLASS == 64
+# define ELF_TARG_CLASS	ELFCLASS64
+#else
+# error wtf??
+#endif
+#endif
+
+#endif
---
+++ src/get_elf_arch.c
@@ -0,0 +1,21 @@
+#include <stdio.h>
+#include <fcntl.h>
+#include <elf.h>
+
+int
+main (int argc, char **argv)
+{
+  int fd;
+  Elf32_Ehdr ehdr;
+
+  fd = open (argv[0], O_RDONLY);
+  if (fd == -1)
+    perror ("open");
+
+  if (read (fd, &ehdr, sizeof (ehdr)) != sizeof (ehdr))
+    perror ("read");
+
+  printf ("#define ELF_ARCH %u\n", ehdr.e_machine);
+
+  close (fd);
+}
