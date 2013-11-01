29b7fae0a2ffed9535606ef230bdd3e0964ce265
Robert Millan <rmh@debian.org>
2013-11-01 12:39:06 +0000

    Add INADDR_ALLRPTS_GROUP, INADDR_CARP_GROUP, INADDR_PFSYNC_GROUP and INADDR_ALLMDNS_GROUP magic IPv4 addresses.
    git-svn-id: svn://anonscm.debian.org/glibc-bsd/trunk/freebsd-glue@5116 ae44cbe4-c7d5-0310-ae45-95c72a56cd7d

--- include/netinet/in.h
+++ include/netinet/in.h
@@ -13,4 +13,17 @@
 
 #endif
 
+#ifndef INADDR_ALLRPTS_GROUP
+#define	INADDR_ALLRPTS_GROUP	(u_int32_t)0xe0000016	/* 224.0.0.22, IGMPv3 */
+#endif
+#ifndef INADDR_CARP_GROUP
+#define	INADDR_CARP_GROUP	(u_int32_t)0xe0000012	/* 224.0.0.18 */
+#endif
+#ifndef INADDR_PFSYNC_GROUP
+#define	INADDR_PFSYNC_GROUP	(u_int32_t)0xe00000f0	/* 224.0.0.240 */
+#endif
+#ifndef INADDR_ALLMDNS_GROUP
+#define	INADDR_ALLMDNS_GROUP	(u_int32_t)0xe00000fb	/* 224.0.0.251 */
+#endif
+
 #endif
