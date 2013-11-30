0b8786abb044f9bfe3081267e941ceb3d3ed7644
Robert Millan <rmh@debian.org>
2013-11-30 11:10:08 +0000

    Add strnstr().
    git-svn-id: svn://anonscm.debian.org/glibc-bsd/trunk/freebsd-glue@5194 ae44cbe4-c7d5-0310-ae45-95c72a56cd7d

--- include/string.h
+++ include/string.h
@@ -5,4 +5,5 @@
 size_t strlcat(char * __restrict, const char * __restrict, size_t);
 size_t strlcpy(char * __restrict, const char * __restrict, size_t);
 void strmode(int, char *);
+char *strnstr(const char *big, const char *little, size_t len);
 #endif
