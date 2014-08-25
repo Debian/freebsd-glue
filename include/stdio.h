9501632c9a3982b42df9663bfbe7d37bdbcd0cf3
Christoph Egger <christoph@debian.org>
2014-08-25 20:29:26 +0000

    Actually remove definitions from the header file
    git-svn-id: svn://anonscm.debian.org/glibc-bsd/trunk/freebsd-glue@5501 ae44cbe4-c7d5-0310-ae45-95c72a56cd7d

--- include/stdio.h
+++ include/stdio.h
@@ -12,12 +12,4 @@
 
 #include <sys/cdefs.h>
 
-__BEGIN_DECLS
-FILE *funopen (const void *cookie, int (*readfn)(void *, char *, int),
-	       int (*writefn)(void *, const char *, int),
-	       fpos_t (*seekfn)(void *, fpos_t, int), int (*closefn)(void *));
-FILE *fropen (void *cookie, int (*readfn)(void *, char *, int));
-FILE *fwopen (void *cookie, int (*writefn)(void *, const char *, int));
-__END_DECLS
-
 #endif
